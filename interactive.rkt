4fe8995af4fcdd9dc4093e2bfa6db3ab49e9264e
Gordon Brown <gordon.brown@elastic.co>
2021-08-18 10:08:10 -0600

    Minor interactive improvements

--- interactive.rkt
+++ interactive.rkt
@@ -13,6 +13,7 @@
  start-all
  stop-all
  start
+ stop
  get-node-dir
  get-node-stdout
  get-node-stderr)
@@ -31,14 +32,17 @@
 (define (get-node node-id)
  (send cluster get-node node-id))
  
-(define (start-all)
- (send cluster start-all))
+(define (start-all #:debug-node (debug-node #f) #:reset (reset #f))
+ (send cluster start-all #:debug-node debug-node #:reset reset))
 
 (define (stop-all)
  (send cluster stop-all))
 
 (define (start node-id #:debug (debug #f) #:reset (reset #f))
  (send cluster start-node node-id #:debug debug #:reset reset))
+
+(define (stop node-id)
+ (send cluster stop-node node-id))
 
 (define (get-node-dir node-id)
  (let ([node (get-node node-id)])
